3d05ffdd57ffb79ff585c8fdb39f30b6d1ddb41f
Raffi Hotter <raphael.hotter@gmail.com>
2024-01-23 18:32:08 -0800

    Remove gray matter

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -1,7 +1,6 @@
 import { cache } from "react";
 import fs from "fs/promises";
 import path from "path";
-import matter from "gray-matter";
 
 export interface Post {
   title: string;
@@ -12,22 +11,25 @@ export interface Post {
 export const getPosts = cache(async (): Promise<Post[]> => {
   const posts = await fs.readdir("app/posts/");
 
+  // Map files to an array of promises
   const checkDirPromises = posts.map(async (file): Promise<string | null> => {
     const filePath = path.join("app/posts", file);
     const stats = await fs.stat(filePath);
 
+    // Return the file name if it's a directory, else null
     return stats.isDirectory() ? file : null;
   });
 
+  // Wait for all the checks to complete
   const dirs = (await Promise.all(checkDirPromises)).filter(
     (dir): dir is string => dir !== null
   );
+  console.log({ dirs });
 
   return Promise.all(
     dirs.map(async (dir): Promise<Post> => {
-      const filePath = path.join("app/posts", dir, "page.mdx");
-      const fileContents = await fs.readFile(filePath, "utf8");
-      const { data: metadata } = matter(fileContents);
+      // import the metadata and content from the MDX file
+      const { metadata } = await import(`../app/posts/${dir}/page.mdx`);
 
       return {
         title: metadata.title,
