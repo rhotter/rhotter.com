a6e169f56b66deb6ae0bbc02a4897020e5bd60a0
Raffi Hotter <raphael.hotter@gmail.com>
2025-01-03 00:18:07 -0800

    add draft

--- app/components/PostHeader.tsx
+++ app/components/PostHeader.tsx
@@ -3,15 +3,24 @@ import { formatDate } from "@/lib/formatDate";
 export const PostHeader = ({
   title,
   date,
+  draft,
 }: {
   title?: string;
   date?: string;
+  draft?: boolean;
 }) => {
   if (!title) return null;
 
   return (
     <div className="mb-8">
-      <h2 className="mb-2">{title}</h2>
+      <h2 className="mb-2 flex items-center">
+        {title}{" "}
+        {draft && (
+          <span className="text-gray-500 text-sm bg-gray-100 px-2 py-1 rounded-md ml-4">
+            Draft
+          </span>
+        )}
+      </h2>
       {date && <div className="text-gray-500 text-sm">{formatDate(date)}</div>}
     </div>
   );
